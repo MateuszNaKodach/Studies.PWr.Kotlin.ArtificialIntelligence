068d50b0223602806f8960f813460564f4d01985
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2018-03-19 15:13:49 +0100

    QAP_Lab1 Add abstract SelectionStrategy

--- src/main/kotlin/io/github/nowakprojects/pwr/ai/lab1/domain/Population.kt
+++ src/main/kotlin/io/github/nowakprojects/pwr/ai/lab1/domain/Population.kt
@@ -1,3 +1,6 @@
 package io.github.nowakprojects.pwr.ai.lab1.domain
 
-data class Population<T>(val chromosomes: List<Chromosome<T>>)
+data class Population<T>(val chromosomes: List<Chromosome<T>>){
+    val size: Int
+        get() = chromosomes.size
+}
